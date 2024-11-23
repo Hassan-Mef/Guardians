277665e140a0d74438bf2fd84b19ae1b3618c5de
Hassan Mef <156674177+Hassan-Mef@users.noreply.github.com>
2024-11-23 15:24:52 +0500

    added enemy animations

--- Enemies/SlowEnemy.cs
+++ Enemies/SlowEnemy.cs
@@ -3,15 +3,26 @@ using System;
 
 public partial class SlowEnemy : EnemyBase
 {
+	private AnimationPlayer _animationPlayer = null;
 	public override void _Ready()
 	{
 		base._Ready(); // Call the parent class's _Ready method
 
 		// Customize properties for SlowEnemy
-		Speed = 35.0f; // Reduce speed
-		MaxHealth = 200; // Increase health
-		Damage = 15; // Modify attack damage
-		
+		// Speed = 35.0f; // Reduce speed
+		// MaxHealth = 200; // Increase health
+		// Damage = 15; // Modify attack damage
+
+		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		if (_animationPlayer != null)
+		{
+			_animationPlayer.Play("idel");
+		}
+		else
+		{
+			GD.PrintErr("AnimationPlayer not found for SlowEnemy!");
+		}
+
 
 		// Additional setup for SlowEnemy
 		GD.Print("SlowEnemy ready with custom properties.");
